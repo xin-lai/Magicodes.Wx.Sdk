87d4fedf18ddfed7f2a4e174f3b953a036d224a8
wenqiang.li <codelove1314@live.cn>
2021-02-04 20:43:05 +0800

    修改WxPublicAccountControllerBase

--- src/Magicodes.Wx.PublicAccount.Sdk.AspNet/WxPublicAccountControllerBase.cs
+++ src/Magicodes.Wx.PublicAccount.Sdk.AspNet/WxPublicAccountControllerBase.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 using Magicodes.Wx.PublicAccount.Sdk.Apis.Sns;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
 
@@ -10,6 +12,7 @@ namespace Magicodes.Wx.PublicAccount.Sdk.AspNet
     /// <summary>
     /// 
     /// </summary>
+    [AllowAnonymous]
     public class WxPublicAccountControllerBase : Controller
     {
         /// <summary>
@@ -49,7 +52,7 @@ namespace Magicodes.Wx.PublicAccount.Sdk.AspNet
         /// 获取微信用户信息
         /// </summary>
         /// <returns></returns>
-        protected async System.Threading.Tasks.Task<GetUserInfoApiResult> GetWeChatUserInfoAsync()
+        protected async Task<GetUserInfoApiResult> GetWeChatUserInfoAsync()
         {
             var snsApi =
                Request.HttpContext.RequestServices.GetRequiredService<ISnsApi>();
