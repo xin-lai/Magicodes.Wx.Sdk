573b29b06dfdf072870ed72f6fde7b6814cb2baa
zan.dai <zan.dai@baibaomen.com>
2021-04-19 22:08:14 +0800

    添加Ticket 获取 和获取JSSDK 配置 辅助方法

--- src/Magicodes.Wx.PublicAccount.Sdk/Helper/WxHelper.cs
+++ src/Magicodes.Wx.PublicAccount.Sdk/Helper/WxHelper.cs
@@ -1,6 +1,7 @@
 ﻿using Magicodes.Wx.PublicAccount.Sdk.Apis.Sns;
 using Microsoft.Extensions.Configuration;
 using System;
+using System.Collections;
 using System.Security.Cryptography;
 using System.Text;
 using System.Text.Encodings.Web;
@@ -80,6 +81,113 @@ namespace Magicodes.Wx.PublicAccount.Sdk
                 Token = wechatConfig["Token"],
                 WeiXinAccount = wechatConfig["WeiXinAccount"],
             };
+        }
+
+        /// <summary>
+        /// 获取时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static string GetTimestamp()
+        {
+            var ts = DateTime.UtcNow - new DateTime(1970, 1, 1, 0, 0, 0, 0);
+            return Convert.ToInt64(ts.TotalSeconds).ToString();
+        }
+
+        /// <summary>
+        /// sha1加密
+        /// </summary>
+        /// <param name="parameters">The parameters<see cref="Hashtable"/></param>
+        /// <returns></returns>
+        private static string CreateSha1(Hashtable parameters)
+        {
+            var sb = new StringBuilder();
+            var akeys = new ArrayList(parameters.Keys);
+            akeys.Sort();
+
+            foreach (var k in akeys)
+                if (parameters[k] != null)
+                {
+                    var v = (string)parameters[k];
+
+                    if (sb.Length == 0)
+                        sb.Append(k + "=" + v);
+                    else
+                        sb.Append("&" + k + "=" + v);
+                }
+            return GetSha1(sb.ToString()).ToLower();
+        }
+
+        /// <summary>
+        /// 生成cardSign的加密方法
+        /// </summary>
+        /// <param name="parameters">The parameters<see cref="Hashtable"/></param>
+        /// <returns></returns>
+        private static string CreateCardSha1(Hashtable parameters)
+        {
+            var sb = new StringBuilder();
+            //此处需要根据参数值进行排序
+            var aValues = new ArrayList(parameters.Values);
+            aValues.Sort();
+
+            foreach (var value in aValues)
+                if (value != null)
+                    sb.Append(value);
+            return GetSha1(sb.ToString()).ToLower();
+        }
+
+        /// <summary>
+        /// 添加卡券Ext参数的签名加密方法
+        /// </summary>
+        /// <param name="parameters"></param>
+        /// <returns></returns>
+        private static string CreateNonekeySha1(Hashtable parameters)
+        {
+            var sb = new StringBuilder();
+            var aValues = new ArrayList(parameters.Values);
+            aValues.Sort();
+
+            foreach (var v in aValues)
+                sb.Append(v);
+            return GetSha1(sb.ToString()).ToLower();
+        }
+
+        /// <summary>
+        /// 获取JS-SDK权限验证的签名Signature
+        /// </summary>
+        /// <param name="ticket"></param>
+        /// <param name="noncestr"></param>
+        /// <param name="timestamp"></param>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        public static string GetSignature(string ticket, string noncestr, string timestamp, string url)
+        {
+            var parameters = new Hashtable
+            {
+                {"jsapi_ticket", ticket},
+                {"noncestr", noncestr},
+                {"timestamp", timestamp},
+                {"url", url}
+            };
+            return CreateSha1(parameters);
+        }
+
+        /// <summary>
+        /// 签名算法
+        /// </summary>
+        /// <param name="str"></param>
+        /// <returns></returns>
+        public static string GetSha1(string str)
+        {
+            //建立SHA1对象
+            SHA1 sha = new SHA1CryptoServiceProvider();
+            //将mystr转换成byte[] 
+            var enc = new ASCIIEncoding();
+            var dataToHash = enc.GetBytes(str);
+            //Hash运算
+            var dataHashed = sha.ComputeHash(dataToHash);
+            //将运算结果转换成string
+            var hash = BitConverter.ToString(dataHashed).Replace("-", "");
+            return hash;
         }
     }
 }
